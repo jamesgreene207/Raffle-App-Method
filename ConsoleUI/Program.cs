a7a69a3e84ddd8ec4929829dde4268f54ab32d78
James <jamesgreene207@hotmail.com>
2021-06-23 22:54:45 -0400

    completed code but without all methods

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,63 +11,153 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-
-
-        }
-
-        //Start writing your code here
-
-
-
-
-
-
-        static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
-        {
-            var counter = 0;
-            for (int i = 0; i < 30; i++)
+            Console.WriteLine("Welcome to the Party!!");
+            //Methods 1-4
+            string name;
+            string otherGuest;
+            do
             {
-                Console.Clear();
+                name = GetUserInput("Please enter your name.");
+                raffleNumber = GenerateRandomNumber(min, max);
+                AddGuestsInRaffle(raffleNumber, name);
+                otherGuest = GetUserInput("Do you want to add another name? ").ToLower();
 
-                switch (counter % 4)
-                {
-                    case 0:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    │││ \\   ║");
-                            Console.WriteLine("         ║    │││  O  ║");
-                            Console.WriteLine("         ║    OOO     ║");
-                            break;
-                        };
-                    case 1:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    OOOO    ║");
-                            break;
-                        };
-                    case 2:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║   / │││    ║");
-                            Console.WriteLine("         ║  O  │││    ║");
-                            Console.WriteLine("         ║     OOO    ║");
-                            break;
-                        };
-                    case 3:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    OOOO    ║");
-                            break;
-                        };
-                }
+            } while (otherGuest == "yes");
 
-                counter++;
-                Thread.Sleep(200);
+            //Method 5
+            foreach (var numAndName in guests)
+            {
+                Console.WriteLine($"{numAndName.Key} : {numAndName.Value}");
             }
+
+
+            //Method 6
+            List<int> keyList = new List<int>(guests.Keys);
+            int winnerNumber = keyList[_rdm.Next(keyList.Count)];
+            //Method 7
+            PrintWinner(winnerNumber);
+            
+
+
+            Console.ReadLine();
         }
+
+        private static Dictionary<int, string> guests = new Dictionary<int, string>();
+        private static int min = 1000;
+        private static int max = 9999;
+        private static int raffleNumber;
+        private static Random _rdm = new Random();
+
+        //Method 1
+        private static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string input;
+            input = Console.ReadLine();
+            return input;
+        }
+        //Method 2
+        //static KeyValuePair<int, string> GetUserInfo(string guest)
+        //{
+        //    string name; 
+        //    string otherGuest;
+        //    do
+        //    {
+        //        name = GetUserInput("Please enter your name.");
+        //        raffleNumber = GenerateRandomNumber(min, max);
+        //        AddGuestsInRaffle(raffleNumber, name);
+
+        //        otherGuest = GetUserInput("Do you want to add another name? ").ToLower();
+
+
+        //    } while (otherGuest == "yes");
+
+        //}
+        //Method 3
+        private static int GenerateRandomNumber(int min, int max)
+        {
+            raffleNumber = _rdm.Next(min, max);
+            return raffleNumber;
+        }
+
+        //Method 4
+        static void AddGuestsInRaffle(int raffleNumber, string guest)
+        {
+            guests.Add(raffleNumber, guest);
+        }
+        //Method 5
+        //private static void PrintGuestsName(Dictionary<int, string>)
+        //{
+        ////    foreach(var numAndName in guests)
+        ////    {
+        ////        Console.WriteLine($"{numAndName.Key} : {numAndName.Value}");
+        ////    }
+        //}
+        //Method 6
+        //private static int GetRaffleNumber(Dictionary<int, string>)
+        //{
+        //    int keyNum = num.Key;
+        //    return keyNum;
+        //}
+
+        //Method 7
+        static void PrintWinner(int winnerNumber)
+        {
+            Console.WriteLine($"The Winner is: {guests[winnerNumber]} with the #{winnerNumber}!");
+        }
+
     }
-}
+
+
+
+
+
+       //static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
+       // {
+       //     var counter = 0;
+       //     for (int i = 0; i < 30; i++)
+       //     {
+       //         Console.Clear();
+
+       //         switch (counter % 4)
+       //         {
+       //             case 0:
+       //                 {
+       //                     Console.WriteLine("         ╔════╤╤╤╤════╗");
+       //                     Console.WriteLine("         ║    │││ \\   ║");
+       //                     Console.WriteLine("         ║    │││  O  ║");
+       //                     Console.WriteLine("         ║    OOO     ║");
+       //                     break;
+       //                 };
+       //             case 1:
+       //                 {
+       //                     Console.WriteLine("         ╔════╤╤╤╤════╗");
+       //                     Console.WriteLine("         ║    ││││    ║");
+       //                     Console.WriteLine("         ║    ││││    ║");
+       //                     Console.WriteLine("         ║    OOOO    ║");
+       //                     break;
+       //                 };
+       //             case 2:
+       //                 {
+       //                     Console.WriteLine("         ╔════╤╤╤╤════╗");
+       //                     Console.WriteLine("         ║   / │││    ║");
+       //                     Console.WriteLine("         ║  O  │││    ║");
+       //                     Console.WriteLine("         ║     OOO    ║");
+       //                     break;
+       //                 };
+       //             case 3:
+       //                 {
+       //                     Console.WriteLine("         ╔════╤╤╤╤════╗");
+       //                     Console.WriteLine("         ║    ││││    ║");
+       //                     Console.WriteLine("         ║    ││││    ║");
+       //                     Console.WriteLine("         ║    OOOO    ║");
+       //                     break;
+       //                 };
+       //         }
+
+       //         counter++;
+       //         Thread.Sleep(200);
+       //     }
+        //}
+  }
+
